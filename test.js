d9e35e2c7b128600ef54c58cac42869f05a22547
christsai1972 <103104560+christsai1972@users.noreply.github.com>
2022-06-28 14:53:10 +0800

    some modify

--- test.js
+++ test.js
@@ -41,10 +41,6 @@ class CPerson {
   }
 }
 
-// CPerson.prototype.sayHi = function () {
-//   console.log(this.name + " says hi.");
-// };
-
 class CSuperman extends CPerson {
   constructor(skill, weapon) {
     super("Superman", 190, 85);
